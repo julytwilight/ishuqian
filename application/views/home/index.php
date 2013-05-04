c549f2bed4364a8ad602bd21ed7870a31ce835dc
2forVendetta <julytwilight@msn.com>
2013-05-04 19:47:37 +0800

    add img

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,129 @@
-<script type="text/javascript" src="js/jquery.simple-color.js"></script>
+<!-- <script type="text/javascript" src="js/jquery.simple-color.js"></script> -->
 <script type='text/javascript' src='js/js.js'></script>
 <script type='text/javascript' src='js/move.js'></script>
+
+<!--输入url-->
+<div id="MarkList" class="MarkList">
+	<h3>创建新书签</h3>
+    <div class="markCont">
+    	<p><span>链接：</span><input class='bookmark_link' name='url' type="text" /></p>
+    	<p class="markTure">
+    	   <input type="button" class="btn_bookmark_link markSub" value="提交"/>
+    	   <input type="button" class="markCancel" id="markCancel" value="取消"/>
+    	</p>
+    </div>
+</div>
+<!--创建书签-->
+<div id="CreateList" class="MarkList">
+	<h3>创建新书签</h3>
+    <div class="markCont">
+    	<?= form_open('bookmarks/create', array('name'=>'form_add_bookmark')) ?>
+	    	<p><span>链接：</span><input name='url' type="text" /></p>
+	        <p><span>标题：</span><input name='title' type="text" /></p>
+	        <p><span>分类：
+	        	<select name='list' style='display:inline'>
+		        	<option value='0'>无分类</option>
+		        	<?php foreach ($lists as $key => $value): ?>
+		        		<option value='<?=$value['id']?>'><?=$value['name']?></option>
+		        	<?php endforeach ?>
+	        	</select>
+	        </p>
+	        <p><span>私有：</span><input name='is_private' type='checkbox' /><em>选中别人无法浏览</em></p>
+	        <p><span>备注：</span><textarea name='commnet'></textarea></p>
+	        <p class="markTure">
+	           <input type="submit" class="markSub" value="提交"/>
+	           <input type="button" class="markCancel" id="markCancel" value="取消"/>
+	        </p>
+        </form>
+    </div>
+</div>
+
+ <div id="ClassList" class="MarkList">
+	<h3>添加新分类</h3>
+    <div class="markCont classCont">
+    	<p><span>分类名称：</span><input type="text" /></p>
+        <p><span>分类颜色：</span><input class='simple_color_kitchen_sink' value='#993300'/></p>
+        <p class="markTure">
+           <input type="submit" class="markSub" value="提交"/>
+           <input type="button" class="markCancel" id="classCancel" value="取消"/>
+        </p>
+    </div>
+</div>
+<div class="mask" id="addMask"></div>
+<div class="mask" id="classMask"></div>
+<div class="mask" id="inMask"></div>
+ <div id="InmarkList" class="MarkList">
+	<h3>导入书签</h3>
+    <div class="markCont inCont">
+    	<p><span>链接：</span><input type="file" class="hide"/>
+        </p>
+        <p class="markTure">
+           <input type="submit" class="markSub" value="提交"/>
+           <input type="button" class="markCancel" id="inCancel" value="取消"/>
+        </p>
+    </div>
+</div>
+
+<div id='p_content' class='clearfix'>
+	<div id='p_leftcon'>
+		<div id='p_prebg'>
+			<div id="grzx">
+				<div id="p_gl"></div>
+				<div id="p_gz">个人中心</div>
+				<div id="p_gr"></div>
+			</div>
+			<div id='p_himg'>
+				<img src='imgs/prehead.png'>
+			</div>
+			<div id='p_hname'>jammy</div>
+			<div id='p_lfixed'>
+                <div id='p_dcr'>
+					<ul>
+						<li id="addMark">添加书签</li>
+						<li id="addClass">添加分类</li>
+						<li id="inMark">导入书签</li>
+					</ul>
+				</div>
+                <div id='p_fl'>
+                    <ul>
+                        <li>
+                            <div class='li_l' style="background:#bdb9b8"></div>
+                            <div class='li_r' style="background:#e0dedf"><a href="home/favorites_list/">全部分类</a></div>
+                        </li>
+                        <?php foreach ($lists as $key => $value): ?>
+                        	<li>
+                        	    <div class='li_l' style="background:#bdb9b8"></div>
+                        	    <div class='li_r' style="background:#e0dedf"><a href="home/favorites_list/"><a href="home/favorites_list/<?=$value['id']?>"><?=$value['name']?></a></a></div>
+                        	</li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</div>
+	<div id='p_rightcon'>
+		<ul>
+			<? foreach ($bookmarks as $key => $value): ?>
+				<a href="" target='_blank'><li></li></a>
+				<li class='p_conlist'>
+				    <ul>
+				      <li></li>
+				      <li></li>
+				      <li></li>
+				      <li></li>
+				    </ul> 
+				    <div class='rlitop'>
+				       <p><img src="imgs/favicon.png" width="16" height="16"><br><a href="<?=$value['url']?>"><?=$value['title']?></a></p>
+				    </div>
+				    <div class='rlimid'>浏览<span>[931]</span> 赞<span>[89]</span></div>
+				    <div class='rlibottom'></div>
+				</li>
+			<? endforeach ?>
+		</ul>
+    </div>
+</div>
+
+<!--
 <div class='left fleft' style='width:300px; border:solid 1px; margin-top:20px; margin-left: 20px;padding:20px'>
 	<a href='javascript:' class='add_bookmark'>add a bookmark</a>
 
@@ -10,14 +133,14 @@
 	</div>
 
 	<div class='bookmark_setting hide'>
-		<?= form_open('bookmarks/create', array('name'=>'form_add_bookmark')) ?>
+		<?//= form_open('bookmarks/create', array('name'=>'form_add_bookmark')) ?>
 			url: <input name='url' /><br />
 			title: <input name='title' /><br />
 			list: <select name='list'>
 					<option value='0'>无分类</option>
-					<?php foreach ($lists as $key => $value): ?>
-						<option value='<?=$value['id']?>'><?=$value['name']?></option>
-					<?php endforeach ?>
+					<?//php foreach ($lists as $key => $value): ?>
+						<option value='<//?=$value['id']?>'><?//=$value['name']?></option>
+					<?//php endforeach ?>
 				 </select><br />
 			private: <input name='is_private' type='checkbox' /><br />
 			comment: <textarea name='commnet'></textarea>
@@ -33,27 +156,24 @@
 		</div>
 		<ul class='lists'>
 			<a href="home/favorites_list/"><li>all</li></a>
-			<?php foreach ($lists as $key => $value): ?>
-				<a href="home/favorites_list/<?=$value['id']?>"><li><?=$value['name']?></li></a>
-			<?php endforeach ?>
+			<?//php foreach ($lists as $key => $value): ?>
+				<a href="home/favorites_list/<?//=$value['id']?>"><li><?//=$value['name']?></li></a>
+			<?//php endforeach ?>
 		</ul>
 	</div>
 </div>
 <div class='bookmarks fleft' style='margin-top:20px; margin-left:20px; border:solid 1px; width:500px'>
 	Bookmarks:
 	<ul class=''>
-		<?php foreach ($bookmarks as $key => $value): ?>
-			<a href="<?=$value['url']?>" target='_blank'><li><?=$value['title']?></li></a>
-		<?php endforeach ?>
+		<?//php foreach ($bookmarks as $key => $value): ?>
+			<a href="<?//=$value['url']?>" target='_blank'><li><?//=$value['title']?></li></a>
+		<//?php endforeach ?>
 	</ul>
 </div>
-
+-->
 <script type="text/javascript">
 	
 	//add bookmark
-	$('.add_bookmark').click(function(){
-		$('.input_add_bookmark').toggle();
-	});
 	$('.btn_bookmark_link').click(function(){
 		var is = true, link = $('.bookmark_link').val();
 		if (is && link) {
@@ -62,12 +182,13 @@
 				$('.add_bookmark').click();
 				$('input[name=title]').val(data);
 				$('input[name=url]').val(link);
-				$('.bookmark_setting').show();
+				$('.markCancel').click();
+				//$('#CreateList').show();
+				woyun('#CreateList','#markCancel')
 				is = true;
 			});
 		}
 	});
-
 
 	//add list
 	$('.add_list').click(function(){
@@ -88,4 +209,22 @@
 			}, 'json');
 		}
 	});
+
+	//显示
+	function woyun(Tdiv,clos)
+	{
+		$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
+		$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
+		$(Tdiv).css('display','block');
+		startMove(Tdiv,{opacity:100});
+		$(clos).click(function()
+		{
+			startMove($(Tdiv),{opacity:0},function(){$(Tdiv).css('display','none');})
+		})
+		$(window).on('scroll resize',function()
+		{
+			$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
+			$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
+		})
+	}
 </script>
--- js/js.js
+++ js/js.js
@@ -30,49 +30,49 @@ window.onload=function()
 			lfixed.style.position = 'relative';
 			lfixed.style.width="100%";
 		}
-	 };	
-		function windowT(obj,Tdiv,clos,oMask,fn)
+	};
+	function windowT(obj,Tdiv,clos,oMask,fn)
+	{
+		$(obj).click(function()
 		{
-			$(obj).click(function()
+			$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
+			$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
+			$(obj).addClass('Hactive');
+			$(Tdiv).css('display','block');
+			startMove(Tdiv,{opacity:100});
+			$(oMask).css('width',$(window).width());
+			$(oMask).css('height',$(document).height());
+			$(oMask).css('display','block');
+			if(fn){
+				fn();
+			}
+			$(clos).click(function()
 			{
-				$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
-				$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
-				$(obj).addClass('Hactive');
-				$(Tdiv).css('display','block');
-				startMove(Tdiv,{opacity:100});
-				$(oMask).css('width',$(window).width());
-				$(oMask).css('height',$(document).height());
-				$(oMask).css('display','block');
-				if(fn){
-					fn();
-				}
-				$(clos).click(function()
-				{
-					startMove(Tdiv,{opacity:0},function(){$(Tdiv).css('display','none');})
-					$(oMask).css('display','none');
-					$(obj).removeClass('Hactive');
-					$(obj).addClass('p_bgco');
-				})
+				startMove(Tdiv,{opacity:0},function(){$(Tdiv).css('display','none');})
+				$(oMask).css('display','none');
+				$(obj).removeClass('Hactive');
+				$(obj).addClass('p_bgco');
 			})
-			$(window).on('scroll resize',function()
-			{
-					$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
-					$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
-					$(oMask).css('width',$(window).width());
-					$(oMask).css('height',$(document).height());
-			})
-	  }
-	  windowT(oaddMark,oMarkList,omarkCancel,oaddMask,function(){$(oaddClass).removeClass('p_bgco');$(oinMark).removeClass('p_bgco');});	
-	  windowT(oaddClass,oClassList,oclassCancel,oclassMask,function(){$(oaddMark).removeClass('p_bgco');$(oinMark).removeClass('p_bgco');});	
-	  windowT(oinMark,oInmarkList,oinCancel,oinMask,function(){$(oaddClass).removeClass('p_bgco');$(oaddMark).removeClass('p_bgco');});	
-	 
-	$('.simple_color_kitchen_sink').simpleColor({
-	   cellWidth: 20,
-	   cellHeight: 20,
-	   border: '1px solid #660033',
-	   buttonClass: 'button',
-	   displayColorCode: true,
-	   livePreview: true,
-	   displayColorCode: false
-	});
+		})
+		$(window).on('scroll resize',function()
+		{
+			$(Tdiv).css('left',($(window).width()-$(Tdiv).outerWidth())/2);
+			$(Tdiv).css('top',($(window).height()-$(Tdiv).outerHeight())/2+$(window).scrollTop());
+			$(oMask).css('width',$(window).width());
+			$(oMask).css('height',$(document).height());
+		})
+	}
+	windowT(oaddMark,oMarkList,omarkCancel,oaddMask,function(){$(oaddClass).removeClass('p_bgco');$(oinMark).removeClass('p_bgco');});	
+	windowT(oaddClass,oClassList,oclassCancel,oclassMask,function(){$(oaddMark).removeClass('p_bgco');$(oinMark).removeClass('p_bgco');});	
+	windowT(oinMark,oInmarkList,oinCancel,oinMask,function(){$(oaddClass).removeClass('p_bgco');$(oaddMark).removeClass('p_bgco');});	
+
+	// $('.simple_color_kitchen_sink').simpleColor({
+	// 	cellWidth: 20,
+	// 	cellHeight: 20,
+	// 	border: '1px solid #660033',
+	// 	buttonClass: 'button',
+	// 	displayColorCode: true,
+	// 	livePreview: true,
+	// 	displayColorCode: false
+	// });
 }
